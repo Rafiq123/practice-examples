9cc9154db66491722daabe55a507024f1c315ca0
Rafikahamad Jamakhane <rjamakhane@apttus.com>
2019-09-30 12:31:13 +0530

    Update

--- tricky-challenges/app.js
+++ tricky-challenges/app.js
@@ -128,6 +128,25 @@
 
 }());
 
+//Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+//For example, arr = [1,3,5,7,9] . Our minimum sum is 1+3+5+7 = 16 and our maximum sum is 3+5+7+9=24 . We would print
+function miniMaxSum(arr) {
+    let sum = 0;
+    let min = arr[0];
+    let max = arr[0];
+    for(let i=0;i<arr.length;i++){
+        sum += arr[i]; 
+        if(arr[i] < min){
+            min = arr[i]
+        }
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+
+    console.log(sum-max, sum - min)
+}
+
 
 
 
